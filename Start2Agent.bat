d56470534610c2a40269225a0cc227b8a49dee2a
sachag678 <sacha.gunaratne@gmail.com>
2017-05-18 15:28:38 -0400

    Updated config file to reflect the new jLOAF changes

--- Start2Agent.bat
+++ Start2Agent.bat
@@ -1,5 +1,5 @@
 start java -cp jLOAF.jar;.;jLOAF-Robocup.jar;. Krislet -team mean -cb cb_react_all_flags_ls_new.cb
 ping localhost
-start java -cp jLOAF.jar;.;jLOAF-Robocup.jar;. Krislet -team Greedy -matchType gmm -cb cb_react_all_flags_rs_new.cb
+start java -cp jLOAF.jar;.;jLOAF-Robocup.jar;. Krislet -team Greedy -cb cb_react_all_flags_rs_new.cb
 ping localhost
 
