4092e41ed90bc3bdb87c59f4f9d88e1529991e68
Unknown <pgavigan@gmail.com>
2019-02-07 10:55:51 -0500

    Corrected issue in behaviour model

--- Behaviour/Behaviour.java
+++ Behaviour/Behaviour.java
@@ -4,8 +4,8 @@ public abstract class Behaviour {
 
 	public static final int turnP = 0;
 	public static final int turnN = 1;
-	public static final int kick = 2;
-	public static final int dash = 3;
+	public static final int dash = 2;
+	public static final int kick = 3;
 	
 	public static final int turnAngle = 10;
 	
